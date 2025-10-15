f6d40acdc68c4a8a2d6a30213aaafd1df6173d8c
Achmat Fauzi <achmatfauzi86@gmail.com>
2025-10-15 13:43:17 +0700

    update collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -9,11 +9,21 @@ public class CollisionHandler : MonoBehaviour
   [SerializeField] ParticleSystem explosionParticle;
   [SerializeField] ParticleSystem finishParticle;
 
+  float timeout = 2.0f;
+
+  bool isControllable = true;
+
+  bool isCollisionActive = true;
+
   private void OnCollisionEnter(Collision collision)
   {
 
-    Debug.Log(collision.gameObject);
     Debug.Log(collision.gameObject.tag);
+
+    if(isCollisionActive == false || isControllable == false)
+    {
+      return;
+    }
 
     switch (collision.gameObject.tag)
     {
@@ -22,18 +32,50 @@ public class CollisionHandler : MonoBehaviour
         break;
 
       case "Finish":
+        // disable kontrol roket
+        isControllable = false;
+        GetComponent<Movement>().enabled = false;
+
         Debug.Log("You Win!");
         finishParticle.Play();
         GetComponent<AudioSource>().PlayOneShot(finishSound);
+        
+        Invoke("NextLevel", timeout);
         break;
 
       default:
+        isCollisionActive = false;
+        GetComponent<Movement>().enabled = false; // matikan script movement
+
         Debug.Log("Crash!!");
         explosionParticle.Play();
-        GetComponent<Movement>().enabled = false; // matikan script movement
         GetComponent<AudioSource>().PlayOneShot(crashSound);
+
+        Invoke("RestartLevel", timeout);
         break;
     }
 
+  }
+
+  private void NextLevel()
+  {
+    // load next level
+    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    int nextLevel = currentSceneIndex + 1;
+
+    // kalau udah di level terakhir, balik ke level pertama
+    if (nextLevel == SceneManager.sceneCountInBuildSettings)
+    {
+      SceneManager.LoadScene(0);
+    }
+    
+    // ke level selanjutnya
+    SceneManager.LoadScene(nextLevel);
+  }
+
+  private void RestartLevel()
+  {
+    // nge restart level jika crash
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
   }
 }
